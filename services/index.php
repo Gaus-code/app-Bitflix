5b77cfd232c46e8bee810f68885f25974a5a9371
Yulya Gaus <yulya.yulya.010202@gmail.com>
2023-11-22 16:19:42 +0300

    add movieCard.php to generate cards

--- services/index.php
+++ services/index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/movieCard.php';
 require_once __DIR__ . '/view.php';
 require_once __DIR__ . '/template.php';
 require_once __DIR__ . '/config.php';
--- views/pages/main.php
+++ views/pages/main.php
@@ -9,28 +9,7 @@ require_once "./../data/movies-data.php";
 	<div class="main__container">
 		<div class="cards__container">
 			<?php foreach ($movies as $movie): ?>
-				<div class="card">
-					<img src="<?= './assets/images/' . $movie['id'] . '.jpg';?>" alt="no time to die" class="card__image">
-					<div class="card__article">
-						<h2 class="card__title"><?= $movie['title'] . ' (' . $movie['release-date'] . ')'?></h2>
-						<h3 class="card__engTittle"><?= $movie['original-title']?></h3>
-						<div class="card__line"></div>
-						<p class="card__description">
-							<?= $movie['description']?>
-						</p>
-						<div class="card__footer">
-							<div class="card__footer_duration">
-								<img src="../../assets/images/clockIcon.svg" alt="clock icon" class="card__footer_duration_icon">
-								<p class="card__footer_duration_time"><?= $movie['duration'] ?> мин. / <?= convertMinutesToHours($movie['duration'])?></p>
-							</div>
-							<p class="card__footer_genre">
-								<?= implode(", ", $movie['genres']) . "..."; ?>
-							</p>
-						</div>
-					</div>
-					<div class="overlay"></div>
-					<div class="card__button"><a class="card__link" href="<?= 'detail.php?ID='. $movie['id'] ?>">Подробнее</a></div>
-				</div>
+				<?= generateMovieCard($movie)?>
 			<?php endforeach; ?>
 		</div>
 	</div>
